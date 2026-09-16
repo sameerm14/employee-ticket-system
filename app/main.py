1616287e9c894af025974e50bcff1d54a42a8f1a
sameerm14 <sameernadaf5761@gmail.com>
2026-09-16 13:21:26 +0530

    Allow production frontend CORS

--- app/main.py
+++ app/main.py
@@ -25,7 +25,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+        "http://localhost:5173"
+        "https://employee-ticket-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
