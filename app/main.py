620a0b2ee0ff558a644d9563ed3d8923a9554495
sameerm14 <sameernadaf5761@gmail.com>
2026-09-16 13:27:29 +0530

    Fix production CORS origin

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
+        "http://localhost:5173",
         "https://employee-ticket-frontend.onrender.com",
     ],
     allow_credentials=True,
